d00db1c3c968e6a55783ffd41b8a8b20e1e9ea7d
UzoCode <akosabenedict@gmail.com>
2024-07-20 01:12:47 +0100

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import LandingPage from './components/LandingPage';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
