759f0b75b60a0d2bb990e5f3354f6afb86b05afe
UzoCode <akosabenedict@gmail.com>
2024-07-25 17:34:45 +0100

    Updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';  // Importing the CSS file
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
